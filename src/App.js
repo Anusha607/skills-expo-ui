1df59e3f3d5d281de798878265a0b67515183954
Anusha.T <anusha.t@apcfss.in>
2025-09-22 12:05:26 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import LocalLoginPage from './Components/NavigationPages/LocalLoginPage';
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
         >
           Learn React Hello World
         </a>
+        <LocalLoginPage></LocalLoginPage>
       </header>
     </div>
   );
---
+++ src/Components/NavigationPages/LocalLoginPage.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const LocalLoginPage = () =>{
+    return(
+        <>Hello Anusha </>
+    )
+
+}
+export default LocalLoginPage;
